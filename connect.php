9253625b195bc5136f04bc01eed472171cd1bea0
Zulqarnain-Aptech123 <zulqarnainhyder.aptech@gmail.com>
2025-09-29 12:31:50 +0500

    Add Connection File

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "crud_db") or die("connection Failed");
+
+
+//  if ($conn) {
+//     echo "Connected SuccessFully";
+//  }else
+//  echo "Connection Failed";
