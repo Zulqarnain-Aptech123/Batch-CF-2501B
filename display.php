118b640545f2c61e388cb90c85d551d77f7bdbec
Zulqarnain-Aptech123 <zulqarnainhyder.aptech@gmail.com>
2025-09-29 16:13:56 +0500

    Add Buttons

--- display.php
+++ display.php
@@ -4,75 +4,80 @@
 <html>
 
 <head>
-  <title>Students List</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Students List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
 <body>
-  <h2>Students</h2>
-  <button  class="btn btn-primary " ><a href="create.php" class="text-light">Add New</a></button>
-  <table border="1" cellpadding="10">
-    <!-- <tr>
-      <th>ID</th><th>Student ID</th><th>Name</th><th>Email</th><th>Action</th>
-    </tr> -->
+    <h2 class="text-center">Students</h2>
+    <table border="1" cellpadding="10">
+        <!-- <tr>
+          <th>ID</th><th>Student ID</th><th>Name</th><th>Email</th><th>Action</th>
+        </tr> -->
 
-  </table>
+    </table>
 
 
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">sid</th>
-        <th scope="col">Name</th>
-        <th scope="col">email</th>
-        <th scope="col">batch</th>
-      </tr>
-    </thead>
-    <tbody>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Std Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Batch</th>
+                <th scope="col">Operations</th>
+            </tr>
+        </thead>
+        <tbody>
 
-      <?php
-      $sql = "SELECT * FROM students";
-      $result = mysqli_query($conn, $sql);
-      // if ($result) {
-      //   $row = mysqli_fetch_assoc($result);
-      //   echo $row['sname'];
-      // }
-      if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          $id = $row['sid'];
-          $name = $row['sname'];
-          $email = $row['semail'];
-          $batch = $row['sbatch'];
-          echo ' <tr>
-      <th scope="row">' . $id . '</th>
-      <td>' . $name . '</td>
-      <td>' . $email . '</td>
-      <td>' . $batch . '</td>
-    </tr>';
-        };
-      };
-      ?>
-      <!-- <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+            <?php
+            $sql = "SELECT * FROM students";
+            $result = mysqli_query($conn, $sql);
+            // if ($result) {
+            //   $row = mysqli_fetch_assoc($result);
+            //   echo $row['sname'];
+            // }
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['sid'];
+                    $name = $row['sname'];
+                    $email = $row['semail'];
+                    $batch = $row['sbatch'];
+                    echo ' <tr>
+                <th scope="row">' . $id . '</th>
+                <td>' . $name . '</td>
+                <td>' . $email . '</td>
+                <td>' . $batch . '</td>
+                <td><button class="btn btn-info "><a href="update.php" class="text-light">Update</a></button>
+                <button class="btn btn-danger m-0"><a href="delete.php" class="text-light">Delete</a></button>
+                </td>
+                </tr>';
+                };
+                
+            };
+            ?>
+            <!-- <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td>@social</td>
+        <th scope="row">3</th>
+        <td>John</td>
+        <td>Doe</td>
+        <td>@social</td>
     </tr> -->
-    </tbody>
-  </table>
+        </tbody>
+    </table>
+    <button class="btn btn-primary "><a href="create.php" class="text-light">Add New</a></button>
 
 
 
@@ -82,7 +87,7 @@
 
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
 
